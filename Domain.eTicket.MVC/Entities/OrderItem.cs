ba629600c87021e5ec535950cc820b3da8ef7955
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-06-21 08:12:14 +0300

    Add project files.

---
+++ Domain.eTicket.MVC/Entities/OrderItem.cs
@@ -0,0 +1,5 @@
+﻿namespace Domain.eTicket.MVC.Entities;
+
+public class OrderItem
+{
+}
---
+++ Domain.eTicket.MVC/Enums/MovieCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.eTicket.MVC.Enums;
+
+public enum MovieCategory
+{
+    Action = 1,
+    Comedy,
+    Drama,
+    Documentary,
+    Cartoon,
+    Horror,
+    Fantacy
+}
