e5072d7bf2ab6a5b15b7b039e3bf38f5bda2a5a3
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-06-27 08:05:06 +0300

    added some entities

--- Domain.eTicket.MVC/Entities/Cinema.cs
+++ Domain.eTicket.MVC/Entities/Cinema.cs
@@ -2,4 +2,11 @@
 
 public class Cinema
 {
+    public int CinemaId { get; set; }
+    public string CinemaName { get; set; }
+    public string CinemaDescription { get; set; }
+    public string CinemaLogo { get; set; }
+    public string CinemaLocation { get; set; }
+
+    public ICollection<Movie>? Movies { get; set; }
 }
