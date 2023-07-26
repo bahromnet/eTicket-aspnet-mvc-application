7b05dcabe47a801bba91cd4e9004707ef519dac8
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-07-27 00:08:26 +0300

    added IApplicatiponDbContext

--- Application.eTicket.MVC/Commons/Interfaces/IApplicationDbContext.cs
+++ Application.eTicket.MVC/Commons/Interfaces/IApplicationDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace Application.eTicket.MVC.Commons.Interfaces;
+﻿using Domain.eTicket.MVC.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.eTicket.MVC.Commons.Interfaces;
 
 public interface IApplicationDbContext
 {
-
+    DbSet<Actor> Actors { get; }
+    DbSet<Cinema> Cinemas { get; }
+    DbSet<Movie> Movies { get; }
+    DbSet<Order> Orders { get; }
+    DbSet<OrderItem> OrderItems { get; }
+    DbSet<Producer> Producers { get; }
 }
