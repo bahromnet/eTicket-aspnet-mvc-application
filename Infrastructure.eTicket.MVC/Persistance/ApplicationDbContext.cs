36561c31383daa3aae5b407fdebbc37bfb726f6f
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-07-28 18:24:00 +0300

    added ConfigureServices.cs

--- Infrastructure.eTicket.MVC/Persistance/ApplicationDbContext.cs
+++ Infrastructure.eTicket.MVC/Persistance/ApplicationDbContext.cs
@@ -1,5 +1,20 @@
-﻿namespace Infrastructure.eTicket.MVC.Persistance;
+﻿using Application.eTicket.MVC.Commons.Interfaces;
+using Domain.eTicket.MVC.Entities;
+using Microsoft.EntityFrameworkCore;
 
-public class ApplicationDbContext
+namespace Infrastructure.eTicket.MVC.Persistance;
+
+public class ApplicationDbContext : IApplicationDbContext
 {
+    public DbSet<Actor> Actors { get; set; }
+
+    public DbSet<Cinema> Cinemas { get; set; }
+
+    public DbSet<Movie> Movies { get; set; }
+
+    public DbSet<Order> Orders { get; set; }
+
+    public DbSet<OrderItem> OrderItems { get; set; }
+
+    public DbSet<Producer> Producers { get; set; }
 }
