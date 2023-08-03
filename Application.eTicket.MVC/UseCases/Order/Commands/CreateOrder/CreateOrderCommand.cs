c9e5eb6b45ea3c73f5163919c39ee33861ea44a8
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-03 23:30:12 +0500

    add orderResponce

--- Application.eTicket.MVC/UseCases/Order/Commands/CreateOrder/CreateOrderCommand.cs
+++ Application.eTicket.MVC/UseCases/Order/Commands/CreateOrder/CreateOrderCommand.cs
@@ -1,4 +1,17 @@
-﻿namespace Application.eTicket.MVC.UseCases.Order.Commands;
-public class CreateOrderCommand
+﻿using MediatR;
+
+namespace Application.eTicket.MVC.UseCases.Order.Commands;
+public record CreateOrderCommand : IRequest<Ulid>
 {
+    public Ulid UserId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public bool IsPaid { get; set; }
+}
+
+public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Ulid>
+{
+    public Task<Ulid> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Application.eTicket.MVC/UseCases/Order/Queries/GetAllOrdersQuery.cs
+++ Application.eTicket.MVC/UseCases/Order/Queries/GetAllOrdersQuery.cs
@@ -1,4 +1,13 @@
-﻿namespace Application.eTicket.MVC.UseCases.Order.Queries;
-public class GetAllOrdersQuery
+﻿using Application.eTicket.MVC.UseCases.Order.Responce;
+using MediatR;
+
+namespace Application.eTicket.MVC.UseCases.Order.Queries;
+public record GetAllOrdersQuery : IRequest<List<OrderResponce>>;
+
+public class GetAllOrdersQueryHandler : IRequestHandler<GetAllOrdersQuery, List<OrderResponce>>
 {
+    public Task<List<OrderResponce>> Handle(GetAllOrdersQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
