2b56012fc2f0fa031656b0081554697d9eb8378a
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-04 22:05:22 +0500

    finished Producer validator

--- Application.eTicket.MVC/UseCases/Producer/Commands/UpdateProducer/UpdateProducerCommandValidator.cs
+++ Application.eTicket.MVC/UseCases/Producer/Commands/UpdateProducer/UpdateProducerCommandValidator.cs
@@ -1,4 +1,44 @@
-﻿namespace Application.eTicket.MVC.UseCases.Producer.Commands.UpdateProducer;
-public class UpdateProducerCommandValidator
+﻿using FluentValidation;
+
+namespace Application.eTicket.MVC.UseCases.Producer.Commands.UpdateProducer;
+public class UpdateProducerCommandValidator : AbstractValidator<UpdateProducerCommand>
 {
+    public UpdateProducerCommandValidator()
+    {
+        RuleFor(command => command.Id)
+            .NotEmpty().WithMessage("Producer ID is required.");
+
+        RuleFor(command => command.ProducerName)
+            .NotEmpty().WithMessage("Producer name is required.")
+            .MaximumLength(100).WithMessage("Producer name must not exceed 100 characters.");
+
+        RuleFor(command => command.ProducerBio)
+            .NotEmpty().WithMessage("Producer bio is required.")
+            .MaximumLength(500).WithMessage("Producer bio must not exceed 500 characters.");
+
+        RuleFor(command => command.ProducerImage)
+            .NotNull().WithMessage("Producer image is required.")
+            .Must(BeAValidImage).WithMessage("Invalid image format. Only JPG, PNG, and JPEG formats are allowed.")
+            .Must(BeAValidImageSize).WithMessage("Image size must not exceed 5MB.");
+    }
+
+    private bool BeAValidImage(string filePath)
+    {
+        if (string.IsNullOrEmpty(filePath))
+            return false;
+
+        var allowedFormats = new[] { ".jpg", ".jpeg", ".png" };
+        var ext = Path.GetExtension(filePath);
+        return allowedFormats.Contains(ext, StringComparer.OrdinalIgnoreCase);
+    }
+
+    private bool BeAValidImageSize(string filePath)
+    {
+        if (string.IsNullOrEmpty(filePath))
+            return false;
+
+        const int maxSize = 5 * 1024 * 1024; // 5MB
+        var fileInfo = new FileInfo(filePath);
+        return fileInfo.Length <= maxSize;
+    }
 }
