55be469f2364218b3cd80cfd900065863293bc7d
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-04 22:59:39 +0500

    Order Validator is finished

--- Application.eTicket.MVC/UseCases/Order/Commands/CreateOrder/CreateOrderCommandValidator.cs
+++ Application.eTicket.MVC/UseCases/Order/Commands/CreateOrder/CreateOrderCommandValidator.cs
@@ -1,4 +1,18 @@
-﻿namespace Application.eTicket.MVC.UseCases.Order.Commands.CreateOrder;
-public class CreateOrderCommandValidator
+﻿using FluentValidation;
+
+namespace Application.eTicket.MVC.UseCases.Order.Commands.CreateOrder;
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
 {
+    public CreateOrderCommandValidator()
+    {
+        RuleFor(command => command.UserId)
+            .NotEmpty().WithMessage("User ID is required.");
+
+        RuleFor(command => command.OrderDate)
+            .NotEmpty().WithMessage("Order date is required.")
+            .LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Order date must be in the past or present.");
+
+        RuleFor(command => command.IsPaid)
+            .NotNull().WithMessage("IsPaid field is required.");
+    }
 }
