01fec5b29065f05a1b0086620d872f9152dee0cd
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-08 21:53:13 +0500

    fixed some classes

--- Application.eTicket.MVC/UseCases/User/Responce/UserResponce.cs
+++ Application.eTicket.MVC/UseCases/User/Responce/UserResponce.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.eTicket.MVC.UseCases.User.Responce;
-internal class UserResponce
+﻿namespace Application.eTicket.MVC.UseCases.User.Responce;
+public class UserResponce
 {
 }
