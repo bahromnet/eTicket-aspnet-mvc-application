cb699f5a8c5806b85d4afe0c027ee9dc48f3e486
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-08 22:15:49 +0500

    fixed some classes

--- Application.eTicket.MVC/UseCases/Permission/Queries/GetByIdPermissionQuery.cs
+++ Application.eTicket.MVC/UseCases/Permission/Queries/GetByIdPermissionQuery.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.eTicket.MVC.UseCases.Permission.Queries;
-internal class GetByIdPermissionQuery
+﻿namespace Application.eTicket.MVC.UseCases.Permission.Queries;
+public class GetByIdPermissionQuery
 {
 }
