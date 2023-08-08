56abbb88c1758b43b400aebe23c248ead401f2c5
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-08 21:50:32 +0500

    fixed some classes

--- Application.eTicket.MVC/UseCases/User/Commands/CreateUser/CreateUserCommand.cs
+++ Application.eTicket.MVC/UseCases/User/Commands/CreateUser/CreateUserCommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.eTicket.MVC.UseCases.User.Commands.CreateUser;
-internal class CreateUserCommand
+﻿namespace Application.eTicket.MVC.UseCases.User.Commands.CreateUser;
+public class CreateUserCommand
 {
 }
--- Application.eTicket.MVC/UseCases/User/Commands/UpdateUser/UpdateUserCommand.cs
+++ Application.eTicket.MVC/UseCases/User/Commands/UpdateUser/UpdateUserCommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.eTicket.MVC.UseCases.User.Commands.UpdateUser;
-internal class UpdateUserCommand
+﻿namespace Application.eTicket.MVC.UseCases.User.Commands.UpdateUser;
+public class UpdateUserCommand
 {
 }
