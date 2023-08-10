5a284c41dda32603187de82fad073ca533c32ab0
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-10 23:34:10 +0500

    Created GetAllRole Mediatr

--- Application.eTicket.MVC/UseCases/Role/Queries/GetAllRolesQuery.cs
+++ Application.eTicket.MVC/UseCases/Role/Queries/GetAllRolesQuery.cs
@@ -1,4 +1,13 @@
-﻿namespace Application.eTicket.MVC.UseCases.Role.Queries;
-public class GetAllRolesQuery
+﻿using Application.eTicket.MVC.UseCases.Role.Responce;
+using MediatR;
+
+namespace Application.eTicket.MVC.UseCases.Role.Queries;
+public record GetAllRolesQuery : IRequest<List<RoleResponce>>;
+
+public class GetAllRolesQueryHandler : IRequestHandler<GetAllRolesQuery, List<RoleResponce>>
 {
+    public Task<List<RoleResponce>> Handle(GetAllRolesQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
