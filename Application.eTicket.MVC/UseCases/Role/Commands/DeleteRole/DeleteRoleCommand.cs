b1fe45cedc8d7f6d303bab29af8dccee42f65df8
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-10 23:27:42 +0500

    created DeleteRole MediatR

--- Application.eTicket.MVC/UseCases/Role/Commands/DeleteRole/DeleteRoleCommand.cs
+++ Application.eTicket.MVC/UseCases/Role/Commands/DeleteRole/DeleteRoleCommand.cs
@@ -1,4 +1,15 @@
-﻿namespace Application.eTicket.MVC.UseCases.Role.Commands.DeleteRole;
-public class DeleteRoleCommand
+﻿using MediatR;
+
+namespace Application.eTicket.MVC.UseCases.Role.Commands.DeleteRole;
+public record DeleteRoleCommand : IRequest
 {
+    public Ulid Id { get; }
+}
+
+public class DeleteRoleCommandHandler : IRequestHandler<DeleteRoleCommand>
+{
+    public Task Handle(DeleteRoleCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
