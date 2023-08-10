262baf018c7cf937d45e5e8263676b039ec4ab53
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-10 23:37:17 +0500

    Created GetByIdRole MediatR

--- Application.eTicket.MVC/UseCases/Role/Queries/GetByIdRoleQuery.cs
+++ Application.eTicket.MVC/UseCases/Role/Queries/GetByIdRoleQuery.cs
@@ -1,4 +1,16 @@
-﻿namespace Application.eTicket.MVC.UseCases.Role.Queries;
-public class GetByIdRoleQuery
+﻿using Application.eTicket.MVC.UseCases.Role.Responce;
+using MediatR;
+
+namespace Application.eTicket.MVC.UseCases.Role.Queries;
+public record GetByIdRoleQuery : IRequest<RoleResponce>
 {
+    public Ulid Id { get; }
+}
+
+public class GetByIdRoleQueryHandler : IRequestHandler<GetByIdRoleQuery, RoleResponce>
+{
+    public Task<RoleResponce> Handle(GetByIdRoleQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
