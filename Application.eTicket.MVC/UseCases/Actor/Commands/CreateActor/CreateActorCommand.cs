4cd09f5dd107daf0f462cca248a8d08d133fa6d5
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-13 12:56:12 +0500

    Added Actor Mapping

--- Application.eTicket.MVC/UseCases/Actor/Commands/CreateActor/CreateActorCommand.cs
+++ Application.eTicket.MVC/UseCases/Actor/Commands/CreateActor/CreateActorCommand.cs
@@ -1,11 +1,12 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 
 namespace Application.eTicket.MVC.UseCases.Actor.Commands;
 public record CreateActorCommand : IRequest<Ulid>
 {
-    public string ActorName { get; set; }
-    public string ActorImage { get; set; }
-    public string ActorBio { get; set; }
+    public string ActorName { get; }
+    public IFormFile? ActorImage { get; }
+    public string ActorBio { get; }
 }
 
 public class CreateActorCommandHandler : IRequestHandler<CreateActorCommand, Ulid>
