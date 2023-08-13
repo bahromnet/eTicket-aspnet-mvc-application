f241334afc4a1fb17eb4b7badcb66e0eab26582a
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-13 12:36:37 +0500

    Added Role Validation

--- Application.eTicket.MVC/UseCases/Role/Commands/CreateRole/CreateRoleCommandValidator.cs
+++ Application.eTicket.MVC/UseCases/Role/Commands/CreateRole/CreateRoleCommandValidator.cs
@@ -1,4 +1,13 @@
-﻿namespace Application.eTicket.MVC.UseCases.Role.Commands.CreateRole;
-public class CreateRoleCommandValidator
+﻿using FluentValidation;
+
+namespace Application.eTicket.MVC.UseCases.Role.Commands.CreateRole;
+public class CreateRoleCommandValidator : AbstractValidator<CreateRoleCommand>
 {
+    public CreateRoleCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required")
+            .MinimumLength(3)
+            .MaximumLength(100);
+    }
 }
--- Application.eTicket.MVC/UseCases/Role/Commands/UpdateRole/UpdateRoleCommandValidator.cs
+++ Application.eTicket.MVC/UseCases/Role/Commands/UpdateRole/UpdateRoleCommandValidator.cs
@@ -1,4 +1,13 @@
-﻿namespace Application.eTicket.MVC.UseCases.Role.Commands.UpdateRole;
-public class UpdateRoleCommandValidator
+﻿using FluentValidation;
+
+namespace Application.eTicket.MVC.UseCases.Role.Commands.UpdateRole;
+public class UpdateRoleCommandValidator : AbstractValidator<UpdateRoleCommand>
 {
+    public UpdateRoleCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required")
+            .MinimumLength(3)
+            .MaximumLength(100);
+    }
 }
