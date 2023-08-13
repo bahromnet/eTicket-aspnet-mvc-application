5c2c95e92d6816a6c59f80adbda94b15c0143324
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-13 14:47:37 +0500

    finished GetAllActors MediatR

--- Application.eTicket.MVC/UseCases/Actors/Queries/GetAllActorQuery.cs
+++ Application.eTicket.MVC/UseCases/Actors/Queries/GetAllActorQuery.cs
@@ -1,13 +1,26 @@
-﻿using Application.eTicket.MVC.UseCases.Actors.Responce;
+﻿using Application.eTicket.MVC.Commons.Interfaces;
+using Application.eTicket.MVC.UseCases.Actors.Responce;
+using AutoMapper;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.eTicket.MVC.UseCases.Actors.Queries;
 public record GetAllActorQuery : IRequest<List<ActorResponce>>;
 
 public class GetAllActorQueryHandler : IRequestHandler<GetAllActorQuery, List<ActorResponce>>
 {
-    public Task<List<ActorResponce>> Handle(GetAllActorQuery request, CancellationToken cancellationToken)
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+    public GetAllActorQueryHandler(IApplicationDbContext context, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<List<ActorResponce>> Handle(GetAllActorQuery request, CancellationToken cancellationToken)
+    {
+        var entities = await _context.Roles.ToListAsync(cancellationToken);
+        var result = _mapper.Map<List<ActorResponce>>(entities);
+        return result;
     }
 }
