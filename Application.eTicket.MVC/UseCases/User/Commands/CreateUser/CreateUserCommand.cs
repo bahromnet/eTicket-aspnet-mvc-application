8676f03bd6315d578118deafa365591092cd45aa
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-13 12:22:58 +0500

    Added User UseCases

--- Application.eTicket.MVC/UseCases/User/Commands/CreateUser/CreateUserCommand.cs
+++ Application.eTicket.MVC/UseCases/User/Commands/CreateUser/CreateUserCommand.cs
@@ -1,4 +1,21 @@
-﻿namespace Application.eTicket.MVC.UseCases.User.Commands.CreateUser;
-public class CreateUserCommand
+﻿using MediatR;
+
+namespace Application.eTicket.MVC.UseCases.User.Commands.CreateUser;
+public record CreateUserCommand : IRequest<Ulid>
 {
+    public Ulid Id { get; }
+    public string FullName { get; }
+    public string UserName { get; }
+    public string Phone { get; }
+    public string Email { get; }
+    public string? Picture { get; }
+    public List<string>? Roles { get; }
+}
+
+public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Ulid>
+{
+    public Task<Ulid> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Application.eTicket.MVC/UseCases/User/Queries/GetByIdUserQuery.cs
+++ Application.eTicket.MVC/UseCases/User/Queries/GetByIdUserQuery.cs
@@ -1,4 +1,16 @@
-﻿namespace Application.eTicket.MVC.UseCases.User.Queries;
-public class GetByIdUserQuery
+﻿using Application.eTicket.MVC.UseCases.User.Responce;
+using MediatR;
+
+namespace Application.eTicket.MVC.UseCases.User.Queries;
+public record GetByIdUserQuery : IRequest<UserResponce>
 {
+    public Ulid Id { get; }
+}
+
+public class GetByIdUserQueryHandler : IRequestHandler<GetByIdUserQuery, UserResponce>
+{
+    public Task<UserResponce> Handle(GetByIdUserQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
