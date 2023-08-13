fa996cb0cb93ce06992507df451487d3ad63c56b
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-13 13:49:38 +0500

    finished delete Actor MediatR

--- Application.eTicket.MVC/Commons/Exceptions/NotFoundException.cs
+++ Application.eTicket.MVC/Commons/Exceptions/NotFoundException.cs
@@ -1,4 +1,23 @@
 ﻿namespace Application.eTicket.MVC.Commons.Exceptions;
-public class NotFoundException
+public class NotFoundException : Exception
 {
+    public NotFoundException()
+      : base()
+    {
+    }
+
+    public NotFoundException(string message)
+        : base(message)
+    {
+    }
+
+    public NotFoundException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+
+    public NotFoundException(string name, object key)
+        : base($"Entity \"{name}\" ({key}) was not found.")
+    {
+    }
 }
