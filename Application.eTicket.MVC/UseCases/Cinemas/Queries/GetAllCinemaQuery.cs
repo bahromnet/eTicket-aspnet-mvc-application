2946e58c56ec99e8f52a879dc029e346bafd719c
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-09-03 08:34:50 +0500

    added generic PaginatedList and finished Cinema mediatr

--- Application.eTicket.MVC/UseCases/Cinemas/Queries/GetAllCinemaQuery.cs
+++ Application.eTicket.MVC/UseCases/Cinemas/Queries/GetAllCinemaQuery.cs
@@ -1,13 +1,48 @@
-﻿using Application.eTicket.MVC.UseCases.Cinemas.Responce;
+﻿using Application.eTicket.MVC.Commons.Interfaces;
+using Application.eTicket.MVC.Commons.Models;
+using Application.eTicket.MVC.UseCases.Cinemas.Responce;
+using AutoMapper;
+using Domain.eTicket.MVC.Entities;
 using MediatR;
 
 namespace Application.eTicket.MVC.UseCases.Cinemas.Queries;
-public record GetAllCinemaQuery : IRequest<List<CinemaResponce>>;
+public record GetAllCinemaQuery : IRequest<List<CinemaResponce>>
+{
+    public string? SearchingText { get; set; }
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+}
 
 public class GetAllCinemaQueryHandler : IRequestHandler<GetAllCinemaQuery, List<CinemaResponce>>
 {
-    public Task<List<CinemaResponce>> Handle(GetAllCinemaQuery request, CancellationToken cancellationToken)
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetAllCinemaQueryHandler(IApplicationDbContext context, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<List<CinemaResponce>> Handle(GetAllCinemaQuery request, CancellationToken cancellationToken)
+    {
+        var pageNumber = request.PageNumber;
+        var pageSize = request.PageSize;
+        var searchingText = request.SearchingText?.Trim();
+
+        var allCinemas = _context.Cinemas.AsQueryable();
+
+        if (!string.IsNullOrEmpty(searchingText))
+        {
+            allCinemas = allCinemas.Where(cinema => cinema.CinemaName.ToLower().Contains(searchingText.ToLower()));
+        }
+
+        var paginatedCiemas = await PaginatedList<Cinema>.CreateAsync(allCinemas, pageNumber, pageSize, cancellationToken);
+
+        var cinemaResponseMap = _mapper.Map<List<CinemaResponce>>(paginatedCiemas.Items);
+
+        var responce = new PaginatedList<CinemaResponce>(cinemaResponseMap, paginatedCiemas.TotalCount, request.PageNumber, request.PageSize);
+
+        return responce;
     }
 }
