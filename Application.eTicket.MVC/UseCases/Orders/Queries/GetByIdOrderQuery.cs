5965e0fad124d2ea9cfc6647923b626574ce6446
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-09-21 23:39:27 +0500

    get by id finished of Order

--- Application.eTicket.MVC/UseCases/Orders/Queries/GetByIdOrderQuery.cs
+++ Application.eTicket.MVC/UseCases/Orders/Queries/GetByIdOrderQuery.cs
@@ -1,4 +1,8 @@
-﻿using Application.eTicket.MVC.UseCases.Orders.Responce;
+﻿using Application.eTicket.MVC.Commons.Exceptions;
+using Application.eTicket.MVC.Commons.Interfaces;
+using Application.eTicket.MVC.UseCases.Orders.Responce;
+using AutoMapper;
+using Domain.eTicket.MVC.Entities;
 using MediatR;
 
 namespace Application.eTicket.MVC.UseCases.Orders.Queries;
@@ -9,8 +13,20 @@ public record GetByIdOrderQuery : IRequest<OrderResponce>
 
 public class GetByIdOrderQueryHandler : IRequestHandler<GetByIdOrderQuery, OrderResponce>
 {
-    public Task<OrderResponce> Handle(GetByIdOrderQuery request, CancellationToken cancellationToken)
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetByIdOrderQueryHandler(IApplicationDbContext context, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<OrderResponce> Handle(GetByIdOrderQuery request, CancellationToken cancellationToken)
+    {
+        var foundOrder = await _context.Orders.FindAsync(new object[] { request.Id, cancellationToken }, cancellationToken)
+            ?? throw new NotFoundException(nameof(Order), request.Id);
+
+        return _mapper.Map<OrderResponce>(foundOrder);
     }
 }
